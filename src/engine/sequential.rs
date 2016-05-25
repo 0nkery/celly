d199032ca777403dc0330a5043674e20421fa7cf
Dmitry Shlagoff <wessmoke@gmail.com>
2016-05-25 18:22:31 +0700

    Simple engine.

---
+++ src/engine/sequential.rs
@@ -0,0 +1,22 @@
+use grid::Grid;
+use engine::Engine;
+
+
+pub struct Sequential<G: Grid> {
+    grid: G
+}
+
+
+impl<G: Grid> Engine for Sequential<G> {
+    type Grid = G;
+
+    fn new(grid: G) -> Self {
+        Sequential { grid: grid }
+    }
+
+    fn run_times(&self, times: i64) {
+        for _ in 0..times {
+            self.grid.step();
+        }
+    }
+}
