8bfbbfaa0bd582100f97d2e594f83a76f17cc275
Dmitry Shlagoff <wessmoke@gmail.com>
2016-05-27 00:32:27 +0700

    New trait for parallel computation.

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -7,6 +7,10 @@ pub trait Engine {
     type Grid: Grid;
 
     fn new(grid: Self::Grid) -> Self;
-
     fn run_times(&self, times: i64);
+}
+
+
+pub trait Parallel: Sized {
+    fn into_parts(self) -> Vec<Self>;
 }
