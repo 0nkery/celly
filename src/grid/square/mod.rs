4334f5cf730fffe914056ffea8e7850469080c25
Dmitry Shlagoff <wessmoke@gmail.com>
2016-06-16 20:44:06 +0700

    Removed old repr system.

--- src/grid/square/mod.rs
+++ src/grid/square/mod.rs
@@ -6,14 +6,12 @@ use traits::Grid;
 use traits::Cell;
 use traits::Nhood;
 use traits::Coord;
-use repr::CellRepr;
-use repr::GridRepr;
 
 use self::iter::Iter;
 pub use self::coord::GridCoord;
 
 
-pub struct SquareGrid<'a, C, N>
+pub struct SquareGrid<C, N>
     where C: Cell,
           N: Nhood<Coord=GridCoord> {
 
@@ -21,13 +19,13 @@ pub struct SquareGrid<'a, C, N>
     old_cells: Vec<C>,
     nhood: N,
     neighbors: Vec<Vec<Option<usize>>>,
-    repr: GridRepr<'a, GridCoord>,
+    dimensions: GridCoord,
     rows: i32,
-    cols: i32,
+    cols: i32
 }
 
 
-impl<'a, C, N> SquareGrid<'a, C, N>
+impl<C, N> SquareGrid<C, N>
     where C: Cell,
           N: Nhood<Coord=GridCoord> {
 
@@ -38,16 +36,15 @@ impl<'a, C, N> SquareGrid<'a, C, N>
         let cells = vec![C::default(); len];
         let old_cells = Vec::with_capacity(len);
         let neighbors = Vec::with_capacity(len);
-        let repr = GridRepr::new(rows, cols, None);
 
         let mut grid = SquareGrid {
             cells: cells,
             old_cells: old_cells,
             nhood: nhood,
             neighbors: neighbors,
-            repr: repr,
             rows: rows,
             cols: cols,
+            dimensions: GridCoord::from_2d(cols, rows)
         };
 
         grid.init();
@@ -57,18 +54,13 @@ impl<'a, C, N> SquareGrid<'a, C, N>
 
     fn init(&mut self) {
 
-        for (offset, cell) in self.cells.iter().enumerate() {
+        for offset in 0 .. self.cells.len() {
 
             let coord = GridCoord::from_offset(offset as i32, self.rows, self.cols);
+
             // init neighbors
             let neighbors = self.get_neighbors(&coord);
             self.neighbors.push(neighbors);
-
-            // init representation
-            let mut repr = CellRepr::new(coord, None);
-
-            cell.repr(&mut repr.state);
-            self.repr.cells.push(repr);
         }
     }
 
@@ -100,16 +92,17 @@ impl<'a, C, N> SquareGrid<'a, C, N>
     }
 
     #[inline]
-    pub fn offset<Crd: Coord>(&self, coord: &Crd) -> usize {
+    pub fn offset(&self, coord: &GridCoord) -> usize {
         (coord.y() * self.cols + coord.x()) as usize
     }
 }
 
 
-impl<'a, C, N> Grid for SquareGrid<'a, C, N>
+impl<C, N> Grid for SquareGrid<C, N>
     where C: Cell,
           N: Nhood<Coord=GridCoord> {
 
+    type Cell = C;
     type Coord = GridCoord;
 
     fn step(&mut self) {
@@ -122,33 +115,22 @@ impl<'a, C, N> Grid for SquareGrid<'a, C, N>
 
             let new_cell = cell.step(neighbors_iter);
 
-            // update representation
-            let ref mut cell_repr = self.repr.cells[cell_no];
-            new_cell.repr(&mut cell_repr.state);
-
             self.cells[cell_no] = new_cell;
         }
     }
 
-    fn repr(&self) -> &GridRepr<GridCoord> {
+    fn restore<G: Grid>(&mut self, other: &G) {
 
-        &self.repr
+        debug_assert_eq!(self.dimensions().x(), other.dimensions().x());
+        debug_assert_eq!(self.dimensions().y(), other.dimensions().y());
+
     }
 
-    fn from_repr<'b: 'c, 'c, Crd: Coord>(&'b mut self, repr: &GridRepr<'c, Crd>) {
+    fn cells(&self) -> &Vec<C> {
+        &self.cells
+    }
 
-        debug_assert_eq!(self.rows, repr.rows);
-        debug_assert_eq!(self.cols, repr.cols);
-
-        for cell_repr in repr.cells.iter() {
-
-            let offset = self.offset(&cell_repr.coord);
-
-            let ref mut cell = self.cells[offset];
-            cell.from_repr(&cell_repr.state);
-
-            let ref mut inner_repr = self.repr.cells[offset];
-            cell.repr(&mut inner_repr.state);
-        }
+    fn dimensions(&self) -> Self::Coord {
+        self.dimensions.clone()
     }
 }
