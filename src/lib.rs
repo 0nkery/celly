2f2a511041a5c3154a6844bd7d8910d2010e7e9b
Dmitry Shlagoff <wessmoke@mail.ru>
2016-06-17 17:26:44 +0700

    Traits and test helpers moved to separate files.

--- src/lib.rs
+++ src/lib.rs
@@ -1,61 +1,13 @@
+#![feature(custom_derive, plugin)]
+#![plugin(serde_macros)]
+
+extern crate serde;
+#[cfg(test)]
+extern crate bincode;
+
 pub mod grid;
 pub mod engine;
+pub mod traits;
 
 mod examples;
-
-
-pub mod traits {
-
-    pub trait Cell: Clone + Binary {
-        type Coord: Coord;
-
-        fn step<'a, I>(&'a self, neighbors: I) -> Self
-            where I: Iterator<Item=Option<&'a Self>>;
-
-        fn with_coord<C: Coord>(C) -> Self;
-        fn coord(&self) -> &Self::Coord;
-    }
-
-    pub trait Nhood {
-        type Coord: Coord;
-
-        fn neighbors(&self, &Self::Coord) -> Vec<Self::Coord>;
-
-        fn neighbors_count(&self) -> usize;
-    }
-
-    pub trait Coord: Clone {
-        fn from_2d(x: i32, y: i32) -> Self;
-
-        fn x(&self) -> i32;
-        fn y(&self) -> i32;
-        fn z(&self) -> i32 { 0 }
-    }
-    
-    pub trait Grid {
-        type Cell: Cell;
-        type Coord: Coord;
-
-        fn step(&mut self);
-
-        fn cells(&self) -> &Vec<Self::Cell>;
-        fn dimensions(&self) -> Self::Coord;
-
-        fn restore<G: Grid>(&mut self, &G);
-        fn set_cells(&mut self, Vec<Self::Cell>);
-    }
-
-    pub trait ReprConsumer {
-        fn consume<G: Grid>(&mut self, repr: &G);
-    }
-
-    pub trait Engine {
-        fn run_times(&mut self, times: i64);
-    }
-
-    pub trait Binary {
-        fn binary(&[u8]) -> Self;
-        fn bytes(&self) -> &[u8];
-    }
-}
-
+mod test_helpers;
---
+++ src/traits.rs
@@ -0,0 +1,48 @@
+use serde::{ Serialize, Deserialize };
+
+    pub trait Cell: Clone + Serialize + Deserialize {
+        type Coord: Coord;
+
+        fn step<'a, I>(&'a self, neighbors: I) -> Self
+            where I: Iterator<Item=Option<&'a Self>>;
+
+        fn with_coord<C: Coord>(C) -> Self;
+        fn coord(&self) -> &Self::Coord;
+    }
+
+    pub trait Nhood {
+        type Coord: Coord;
+
+        fn neighbors(&self, &Self::Coord) -> Vec<Self::Coord>;
+
+        fn neighbors_count(&self) -> usize;
+    }
+
+    pub trait Coord: Clone + Serialize + Deserialize {
+        fn from_2d(x: i32, y: i32) -> Self;
+
+        fn x(&self) -> i32;
+        fn y(&self) -> i32;
+        fn z(&self) -> i32 { 0 }
+    }
+    
+    pub trait Grid {
+        type Cell: Cell;
+        type Coord: Coord;
+
+        fn step(&mut self);
+
+        fn cells(&self) -> &Vec<Self::Cell>;
+        fn dimensions(&self) -> Self::Coord;
+
+        fn restore<G: Grid>(&mut self, &G);
+        fn set_cells(&mut self, Vec<Self::Cell>);
+    }
+
+    pub trait ReprConsumer {
+        fn consume<G: Grid>(&mut self, repr: &G);
+    }
+
+    pub trait Engine {
+        fn run_times(&mut self, times: i64);
+    }
