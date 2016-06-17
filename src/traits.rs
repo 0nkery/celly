c42e5f64bdebdf8e4513ca025898c1c7e8113ed4
Dmitry Shlagoff <wessmoke@gmail.com>
2016-06-17 23:52:58 +0700

    Removed 4-space offset.

--- src/traits.rs
+++ src/traits.rs
@@ -1,48 +1,48 @@
 use serde::{ Serialize, Deserialize };
 
-    pub trait Cell: Clone + Serialize + Deserialize {
-        type Coord: Coord;
+pub trait Cell: Clone + Serialize + Deserialize {
+    type Coord: Coord;
 
-        fn step<'a, I>(&'a self, neighbors: I) -> Self
-            where I: Iterator<Item=Option<&'a Self>>;
+    fn step<'a, I>(&'a self, neighbors: I) -> Self
+        where I: Iterator<Item=Option<&'a Self>>;
 
-        fn with_coord<C: Coord>(C) -> Self;
-        fn coord(&self) -> &Self::Coord;
-    }
+    fn with_coord<C: Coord>(C) -> Self;
+    fn coord(&self) -> &Self::Coord;
+}
 
-    pub trait Nhood {
-        type Coord: Coord;
+pub trait Nhood {
+    type Coord: Coord;
 
-        fn neighbors(&self, &Self::Coord) -> Vec<Self::Coord>;
+    fn neighbors(&self, &Self::Coord) -> Vec<Self::Coord>;
 
-        fn neighbors_count(&self) -> usize;
-    }
+    fn neighbors_count(&self) -> usize;
+}
 
-    pub trait Coord: Clone + Serialize + Deserialize {
-        fn from_2d(x: i32, y: i32) -> Self;
+pub trait Coord: Clone + Serialize + Deserialize {
+    fn from_2d(x: i32, y: i32) -> Self;
 
-        fn x(&self) -> i32;
-        fn y(&self) -> i32;
-        fn z(&self) -> i32 { 0 }
-    }
-    
-    pub trait Grid {
-        type Cell: Cell;
-        type Coord: Coord;
+    fn x(&self) -> i32;
+    fn y(&self) -> i32;
+    fn z(&self) -> i32 { 0 }
+}
 
-        fn step(&mut self);
+pub trait Grid {
+    type Cell: Cell;
+    type Coord: Coord;
 
-        fn cells(&self) -> &Vec<Self::Cell>;
-        fn dimensions(&self) -> Self::Coord;
+    fn step(&mut self);
 
-        fn restore<G: Grid>(&mut self, &G);
-        fn set_cells(&mut self, Vec<Self::Cell>);
-    }
+    fn cells(&self) -> &Vec<Self::Cell>;
+    fn dimensions(&self) -> Self::Coord;
 
-    pub trait ReprConsumer {
-        fn consume<G: Grid>(&mut self, repr: &G);
-    }
+    fn restore<G: Grid>(&mut self, &G);
+    fn set_cells(&mut self, Vec<Self::Cell>);
+}
 
-    pub trait Engine {
-        fn run_times(&mut self, times: i64);
-    }
+pub trait ReprConsumer {
+    fn consume<G: Grid>(&mut self, repr: &G);
+}
+
+pub trait Engine {
+    fn run_times(&mut self, times: i64);
+}
