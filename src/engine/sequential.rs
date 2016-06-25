85df9aad678192e2f31fb224a025846525887ab7
Dmitry Shlagoff <wessmoke@gmail.com>
2016-06-25 18:53:47 +0700

    Support for the new constraint.

--- src/engine/sequential.rs
+++ src/engine/sequential.rs
@@ -1,16 +1,17 @@
+use traits::Cell;
+use traits::Consumer;
 use traits::Grid;
 use traits::Engine;
-use traits::Consumer;
 
 
-pub struct Sequential<G: Grid, C: Consumer> {
+pub struct Sequential<C: Cell, G: Grid<Cell=C>, Con: Consumer<Cell=C>> {
     grid: G,
-    consumer: C
+    consumer: Con
 }
 
-impl<G: Grid, C: Consumer> Sequential<G, C> {
+impl<C: Cell, G: Grid<Cell=C>, Con: Consumer<Cell=C>> Sequential<C, G, Con> {
 
-    pub fn new(grid: G, consumer: C) -> Self {
+    pub fn new(grid: G, consumer: Con) -> Self {
         Sequential {
             grid: grid,
             consumer: consumer
@@ -19,7 +20,7 @@ impl<G: Grid, C: Consumer> Sequential<G, C> {
 }
 
 
-impl<G: Grid, C: Consumer> Engine for Sequential<G, C> {
+impl<C: Cell, G: Grid<Cell=C>, Con: Consumer<Cell=C>> Engine for Sequential<C, G, Con> {
     fn run_times(&mut self, times: i64) {
         for _ in 0..times {
             self.grid.step();
