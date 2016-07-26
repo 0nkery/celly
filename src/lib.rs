73f2ea268e8a3d905b7be9b093d9c1f38ad2b77a
Dmitry Shlagoff <wessmoke@gmail.com>
2016-07-27 06:21:48 +0700

    Notes about lib in outline.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,17 @@
 #![plugin(serde_macros)]
 #![warn(missing_docs)]
 
+//! Library for building cellular automata.
+//!
+//! Cellular automata made by supplying some cell to
+//! grid and "engine". Grid stores all cells, chooses
+//! how to iterate them, how to split itself etc.
+//! Engine runs grid evolution in some way and passes
+//! updates to so-called consumers.
+//!
+//! Consumers are interface parts to outer world.
+//! They can be GUI, Web or simple file writers.
+
 extern crate serde;
 #[cfg(test)]
 extern crate bincode;
