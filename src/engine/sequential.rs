63210c683b9d2c8ec5a79fc5827755fbb0ecf539
Dmitry Shlagoff <wessmoke@mail.ru>
2016-07-27 12:49:16 +0700

    Note about struct and constructor.

--- src/engine/sequential.rs
+++ src/engine/sequential.rs
@@ -8,12 +8,16 @@ use traits::Grid;
 use traits::Engine;
 
 
+/// Engine generic over Cell and Consumer running
+/// evolution sequentially.
 pub struct Sequential<C: Cell, G: Grid<Cell = C>, Con: Consumer<Cell = C>> {
     grid: G,
     consumer: Con,
 }
 
 impl<C: Cell, G: Grid<Cell = C>, Con: Consumer<Cell = C>> Sequential<C, G, Con> {
+
+    /// Constructs engine with given Grid and Consumer.
     pub fn new(grid: G, consumer: Con) -> Self {
         Sequential {
             grid: grid,
