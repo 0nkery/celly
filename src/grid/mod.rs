d80ed303c1673cf6be053d13b5f03b695ab2a1ef
Dmitry Shlagoff <wessmoke@mail.ru>
2016-07-27 12:53:01 +0700

    Note about EmptyState.

--- src/grid/mod.rs
+++ src/grid/mod.rs
@@ -4,6 +4,9 @@ pub mod nhood;
 
 use traits::EvolutionState;
 
+/// Dummy evolution state to be used with
+/// cellular automata where this concept is
+/// not applicable.
 pub struct EmptyState;
 
 impl EvolutionState for EmptyState {
